b36b34b52b8a99999c8db7cf1b6672b4e3d03cd5
dieguitodg2014-cloud <dieguitodg2014@gmail.com>
2026-09-18 14:47:39 -0500

    Add Learning Evidence package

---
+++ core/evidence/__init__.py
@@ -0,0 +1,5 @@
+"""Observable learning-evidence primitives."""
+
+from .learning_evidence import EvidenceRecord, InMemoryLearningEvidenceStore, JsonLearningEvidenceStore
+
+__all__ = ["EvidenceRecord", "InMemoryLearningEvidenceStore", "JsonLearningEvidenceStore"]
